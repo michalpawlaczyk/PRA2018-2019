407938a8274786b5aa0e44905d31b3fba153f6e6
michalpawlaczyk <micpaw7@st.amu.edu.pl>
2020-02-29 10:20:35 +0100

    fetch: Finished exercise 1

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,14 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+   final static Logger log = Logger.getLogger("name");
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+        log.info("message");
     }
+
+
 }
